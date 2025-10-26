6666added8bb32acc0d2f4f7c1bd487258ea2b22
meowthcollectable-coder <meowthcollectable@gmail.com>
2025-10-26 19:24:07 +0100

    Delete src directory

--- src/assets.js
+++
@@ -1,5 +0,0 @@
-export const ASSETS = {
-  MAP: "/assets/map.jpg",
-  ISLAND: "/assets/island.png",
-  SHIP: "/assets/ship.png"
-};
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import Control from "./Control";
-import "./styles.css";
-const root = createRoot(document.getElementById("root"));
-const path = window.location.pathname;
-root.render(path.startsWith("/control") ? <Control /> : <App />);
